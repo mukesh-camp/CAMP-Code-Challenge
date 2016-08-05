a35a840dfff61d9261629a94477b40f7699f5f7d
Mukesh Kumar <mukesh.kumar@campsystems.com>
2016-08-05 07:56:56 +0530

    1st Aug Code challenge check in

---
+++ 1Aug2016/1Aug2016/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace Aug1st2016
+{
+    // ReSharper disable once InconsistentNaming
+    public class ProgramAug1st2016
+    {
+        public static void Main(string[] args)
+        {
+            var inputValues = Console.ReadLine();
+            if (inputValues != null)
+            {
+                var elements = inputValues.Split(',');
+                if (elements.Length > 100000)
+                {
+                    Console.WriteLine("No of elements can not exceed 100000");
+                    return;
+                }
+                var numbers = Array.ConvertAll(elements, Int32.Parse);
+                var result = CheckTotal(numbers);
+                Console.WriteLine(result ? "TRUE" : "FALSE");
+           }
+        }
+
+        public static bool CheckTotal(int[] nums)
+        {
+            var leftIndex = 0;
+            var rightIndex = nums.Length - 1;
+
+            var leftTotal = nums[leftIndex];
+            var rightTotal = nums[rightIndex];
+
+            while (leftIndex != rightIndex)
+            {
+                if (leftTotal < rightTotal)
+                {
+                    leftIndex++;
+                    leftTotal += nums[leftIndex];
+                }
+                else if (leftTotal > rightTotal)
+                {
+                    rightIndex--;
+                    rightTotal += nums[rightIndex];
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 1Aug2016/Aug1st2016Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aug1st2016;
+
+namespace Aug1st2016Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var inputVals= new int[5] { 1,2,3,4,5};
+            var result = ProgramAug1st2016.CheckTotal(inputVals);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var inputVals = new int[4] { 11, 20, 8, 31 };
+            var result = ProgramAug1st2016.CheckTotal(inputVals);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var inputVals = new int[7] { 1, 2, 3, 3, 0, 9, 9 };
+            var result = ProgramAug1st2016.CheckTotal(inputVals);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var inputVals = new int[5] { 1, 2, 3, 4, 5 };
+            var result = ProgramAug1st2016.CheckTotal(inputVals);
+            Assert.IsFalse(result);
+        }
+    }
+}
